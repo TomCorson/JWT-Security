5434c7e4006fa4edfb3d865924881d24332d5de0
TomCorson <corsontom@yahoo.com>
2021-02-22 15:22:07 -0500

    JWT entry point, success handler

--- src/main/java/com/JWTSpringSecurity/JWTSecurity/Security/JwtAuthenticationProvider.java
+++ src/main/java/com/JWTSpringSecurity/JWTSecurity/Security/JwtAuthenticationProvider.java
@@ -1,4 +1,17 @@
 package com.JWTSpringSecurity.JWTSecurity.Security;
 
-public class JwtAuthenticationProvider {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
 }
--- src/main/java/com/JWTSpringSecurity/JWTSecurity/Security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/JWTSpringSecurity/JWTSecurity/Security/JwtAuthenticationTokenFilter.java
@@ -1,4 +1,23 @@
 package com.JWTSpringSecurity.JWTSecurity.Security;
 
-public class JwtAuthenticationTokenFilter {
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessingFilter {
+    public JwtAuthenticationTokenFilter() {
+        super("/**");
+    }
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+
 }
